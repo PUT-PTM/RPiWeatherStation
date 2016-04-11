515d6486c9b3a6d765a0bcbaafd17291c5564f04
Wituch <wituch95@poczta.onet.pl>
2016-04-12 00:50:45 +0200

    Added LED's blinking

--- WeatherStation/StartupTask.cs
+++ WeatherStation/StartupTask.cs
@@ -4,22 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using Windows.System.Threading;
 
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
-
-namespace WeatherStation
+namespace BlinkyHeadlessCS
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        BackgroundTaskDeferral deferral;
+        private GpioPinValue value = GpioPinValue.High;
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private ThreadPoolTimer timer;
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            deferral = taskInstance.GetDeferral();
+            InitGPIO();
+            timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromMilliseconds(500));
+
+        }
+        private void InitGPIO()
+        {
+            pin = GpioController.GetDefault().OpenPin(LED_PIN);
+            pin.Write(GpioPinValue.High);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        private void Timer_Tick(ThreadPoolTimer timer)
+        {
+            value = (value == GpioPinValue.High) ? GpioPinValue.Low : GpioPinValue.High;
+            pin.Write(value);
         }
     }
 }
+
